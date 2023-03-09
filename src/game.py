579c06fba5945efe4940391fb2df49d7a0e07df7
Greg Lynskey <gclynskey@outlook.com>
2023-03-08 23:48:56 -0500

    Added fps meter

--- src/game.py
+++ src/game.py
@@ -1,22 +1,30 @@
 import pygame
 import sys
+import debug as dbg
 from settings import *
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN, pygame.RESIZABLE)
-        pygame.display.set_caption("Islet Roasters")
+        pygame.display.set_caption("Islet Roasters: Brewing Away!")
 
         self.clock = pygame.time.Clock()
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    def draw(self):
+        self.screen.fill("black")
+        dbg.debug(f"{self.clock.get_fps() :.1f}")
+
+    def update(self):
+        pygame.display.update()
+        self.clock.tick(FPS)
     def run(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            self.screen.fill("black")
-            pygame.display.update()
-            self.clock.tick(FPS)
+            self.check_events()
+            self.draw()
+            self.update()
 
