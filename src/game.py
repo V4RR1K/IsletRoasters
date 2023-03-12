fee3f612f09e1c60c019f1ded553cfad9de09ab0
Greg Lynskey <gclynskey@outlook.com>
2023-03-11 21:48:54 -0500

    QOL settings update, made settings a class

--- src/game.py
+++ src/game.py
@@ -8,13 +8,18 @@ import sys
 import debug as dbg
 import level
 import fpsmeter as fm
-from settings import *
+import settings as st
 
 class Game:
     def __init__(self):
+        self.settings = st.settings()
         pygame.init()
-        #self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN, pygame.RESIZABLE)
+
+        if self.settings.FULL_SCREEN:
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        else:
+            self.screen = pygame.display.set_mode((self.settings.SCREEN_WIDTH, self.settings.SCREEN_HEIGHT))
+
         pygame.display.set_caption("Islet Roasters: Brewing Away!")
         pygame.display.set_icon(pygame.image.load("assets/player/MainCharacterForwardIdle.png"))
         self.level = level.Level()
